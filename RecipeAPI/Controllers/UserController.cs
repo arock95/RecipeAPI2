b2c9e79dc332096be0343e359d577fa9d265fb44
Anthony Rocchio <anthrock@hotmail.com>
2020-01-12 21:00:34 -0500

    added identity, updated db, added user controller for registratoin

---
+++ RecipeAPI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public UserController()
+        {
+
+        }
+
+        //use recipe user as model here on a post -- well a view model w/ fn, ln, email, pwd
+    }
+}
